3d9dc3df24d825a1fb4483a3d1148a5af72c07ed
gioboa <giorgiob.boa@gmail.com>
2023-09-25 16:49:46 +0200

    add getAccessTokenSilently method

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,35 +1,47 @@
-import React from "react";
-import { Container, Row, Col } from "reactstrap";
-
-import Highlight from "../components/Highlight";
-import Loading from "../components/Loading";
-import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
+import React, { useEffect } from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import Loading from '../components/Loading';
+import { useAuth0, withAuthenticationRequired } from '@auth0/auth0-react';
 
 export const ProfileComponent = () => {
-  const { user } = useAuth0();
+	const { user, getAccessTokenSilently } = useAuth0();
 
-  return (
-    <Container className="mb-5">
-      <Row className="align-items-center profile-header mb-5 text-center text-md-left">
-        <Col md={2}>
-          <img
-            src={user.picture}
-            alt="Profile"
-            className="rounded-circle img-fluid profile-picture mb-3 mb-md-0"
-          />
-        </Col>
-        <Col md>
-          <h2>{user.name}</h2>
-          <p className="lead text-muted">{user.email}</p>
-        </Col>
-      </Row>
-      <Row>
-        <Highlight>{JSON.stringify(user, null, 2)}</Highlight>
-      </Row>
-    </Container>
-  );
+	useEffect(() => {
+		(async () => {
+			try {
+				const token = await getAccessTokenSilently({
+					authorizationParams: { scope: 'read:posts' },
+				});
+				console.log('token', token);
+			} catch (e) {
+				// Handle errors such as `login_required` and `consent_required` by re-prompting for a login
+				console.error(e);
+			}
+		})();
+	}, [getAccessTokenSilently]);
+
+	return (
+		<Container className='mb-5'>
+			<Row className='align-items-center profile-header mb-5 text-center text-md-left'>
+				<Col md={2}>
+					<img
+						src={user.picture}
+						alt='Profile'
+						className='rounded-circle img-fluid profile-picture mb-3 mb-md-0'
+					/>
+				</Col>
+				<Col md>
+					<h2>{user.name}</h2>
+					<p className='lead text-muted'>{user.email}</p>
+				</Col>
+			</Row>
+			<Row>
+				{JSON.stringify(user, null, 2)}
+			</Row>
+		</Container>
+	);
 };
 
 export default withAuthenticationRequired(ProfileComponent, {
-  onRedirecting: () => <Loading />,
+	onRedirecting: () => <Loading />,
 });
