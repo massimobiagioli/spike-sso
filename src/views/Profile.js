209396b6b04dce1af221fefd691a3ae6eb1c46ae
Nicola Corvo <nicola.corvo@it.clara.net>
2023-09-28 16:52:17 +0200

    Edit param

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -9,8 +9,8 @@ export const ProfileComponent = () => {
 	useEffect(() => {
 		(async () => {
 			try {
-				const token = await getAccessTokenSilently({
-					authorizationParams: { scope: "openid+profile+offline_access+name+given_name+family_name+nickname+email+email_verified+picture+created_at+identities+phone+address" },
+				const token = await getAccessTokenSilently({ 
+					scope: "openid+profile+offline_access+name+given_name+family_name+nickname+email+email_verified+picture+created_at+identities+phone+address" 
 				});
 				console.log('token', token);
 			} catch (e) {
