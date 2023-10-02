2d2fce2cf2aa6e59e5b8e4e2826a9fc339ad4804
Nicola Corvo <nicola.corvo@it.clara.net>
2023-10-02 10:02:34 +0200

    Edit params

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const providerConfig = {
   authorizationParams: {
     redirect_uri: "https://main.dh81w1al6xzzw.amplifyapp.com",
     audience: "https://it-portfolio-api",
-    scope: "openid+profile+email+name+given_name+family_name"
+    scope: "profile email name given_name family_name"
   }
 };
 
